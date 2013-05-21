90cf0835b1fe8e1ee36e23a316127420971b29bb
Grant Slater <git@firefishy.com>
2013-05-22 00:03:11 +0100

    Commit very basic working code

--- GeoIPContent.php
+++ GeoIPContent.php
@@ -18,6 +18,13 @@ class plgContentGeoIPContent extends JPlugin {
 	function onContentPrepare( $context, &$article, &$params, $limitstart=0 ) {
 		$pluginParams = $this->params;
 
+		require_once('geoip-api-php/geoip.inc');
+		$maxmind_geoip_file = dirname(__FILE__).'/maxmind-data/GeoIP.dat';
+		$gi = geoip_open($maxmind_geoip_file, GEOIP_STANDARD);
+		$client_ip = $this->getClientIp();
+		$client_ip_code = geoip_country_code_by_addr($gi, $client_ip);
+		geoip_close($gi);
+	
 		$contents = $article->text;
 		$output = '';
 		$regexp = '/(.*?)'.'\{GEO ([\!A-Za-z]{2,4})\}'.'\s*(.*?)'.'\{\/GEO\}'.'(.*)/s';
@@ -25,10 +32,16 @@ class plgContentGeoIPContent extends JPlugin {
 		while ($found) {
 			$output .= $matches[1];
 			$country = $matches[2];
-			$country_contents = '<div class="Country'.$country.'">'.$matches[3].'</div>';
-
-			$output .= $country_contents;
-			
+			if ($country[0] == '!') {
+				if (strtoupper(substr($country,1)) != $client_ip_code) {
+					$output .= $matches[3];
+				}			
+			} else {
+				if (strtoupper($country) == $client_ip_code) {
+					$output .= $matches[3];
+				}
+			}
+	
 			$contents = $matches[4];
 			$found = preg_match($regexp, $contents, $matches);
 		}
@@ -36,4 +49,16 @@ class plgContentGeoIPContent extends JPlugin {
 		$article->text = $output;
 		return true;
 	}
+
+    function getClientIp() {
+		//FIXME handling for proxy chain needed
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 }
